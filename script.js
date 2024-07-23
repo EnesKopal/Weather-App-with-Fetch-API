2c1e82f7162211228bf3c1780809e1735cc3dea3
Enes Kopal <145207995+EnesKopal@users.noreply.github.com>
2024-07-23 14:59:33 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const key= "e198266aaa082e51f38074111437e2f7";
+
+const setQuery=(e)=>{
+    if(e.keyCode=='13'){
+        getResult(searchBar.value);
+    }
+}
+
+const getResult =(cityName)=>{
+   fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${key}&units=metric&lang=tr`)
+   .then(weather=>{
+    return weather.json()
+   })
+   .then(displayResult)
+}
+
+const displayResult=(result)=>{
+    let city = document.querySelector('.city');
+    city.innerText=`${result.name},${result.sys.country}`;
+
+    let temp= document.querySelector('.temp');
+    temp.innerText=`${Math.round(result.main.temp)}°C`;
+
+    let desc= document.querySelector('.desc');
+    desc.innerText=result.weather[0].description;
+    let minmax=document.querySelector('.minmax');
+    minmax.innerText= `${Math.round(result.main.temp_min)}°C / ${Math.round(result.main.temp_max)}°C`
+}
+
+const searchBar =document.getElementById('searchBar');
+searchBar.addEventListener('keypress',setQuery)
